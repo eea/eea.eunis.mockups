20293f7886c53ff1bb2cf507974bee630d964fa4
ichim-david <ichim.david@gmail.com>
2013-05-13 14:15:27 +0200

    Added EEA header and footer to static mockup

---
+++ Gruntfile.js
@@ -0,0 +1,68 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    meta: {
+      version: '0.1.0'
+    },
+    banner: '',
+    // Task configuration.
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: true
+      },
+      dist: {
+        src: ['species/head.html', 
+              'species/header.html', 
+              'species/body.html', 
+              'species/footer.html'],
+
+        dest: 'species/dist/index.html'
+      }
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        browser: true,
+        globals: {
+          jQuery: true
+        }
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib_test: {
+        src: ['species/**/*.js']
+      }
+    },
+    watch: {
+      concat: {
+        files: '<%= concat.dist.src %>',
+        tasks: ['concat']
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task.
+  grunt.registerTask('default', ['concat']);
+
+};
+
