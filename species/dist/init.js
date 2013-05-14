3f62e6f8349af58dd6caffdb48a784321a31f3af
ichim-david <ichim.david@gmail.com>
2013-05-14 16:00:46 +0200

    Refs #14511; Changed the following: - Modified javascript script tags to close as block tags to avoid bug   reported as #14523 - Added init.js in order to initialize any needed js code for species   page - Added style.css for specific or not yet generalized styles - Added test gallery images

---
+++ species/dist/init.js
@@ -0,0 +1,25 @@
+(function($) {
+    $(function() {
+
+        // Species slider gallery
+        var buttons = {
+            previous : $('.gallery-slider-controls .button-prev'),
+            next     : $('.gallery-slider-controls .button-prev')
+        };
+
+        $('.gallery-slider').lofJSidernews(
+            {
+                interval        : 4000,
+                direction		: 'opacity',
+                easing			: 'easeOutBounce',
+                duration		: 1200,
+                opacityClass    : 'lof-opacity',
+                auto            : true,
+                buttons			: buttons,
+                wapperSelector  : '.gallery-slider-wrap-inner',
+                toggleElement   : '#content'
+
+            }
+        );
+    });
+})(jQuery);
