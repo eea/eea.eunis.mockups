72aebff0fca68ac25301290bfd0f7530f2492f18
ichim-david <ichim.david@gmail.com>
2013-06-07 15:18:53 +0300

    Markup(homepage) Watch now only the homepage with grunt and added homepage.css

--- Gruntfile.js
+++ Gruntfile.js
@@ -70,7 +70,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task.
-  grunt.registerTask('default', ['concat']);
+  grunt.registerTask('default', ['concat:homepage']);
 
 };
 
